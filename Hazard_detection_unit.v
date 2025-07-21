2438933455f5beed6ffd07404722e49238b23cd9
Animesh Singh  Sikarwar <animeshstudent@gmail.com>
2025-07-21 23:15:30 +0530

    Update Hazard_detection_unit.v

--- Hazard_detection_unit.v
+++ Hazard_detection_unit.v
@@ -1,66 +1,38 @@
 `timescale 1ns / 1ps
 
     module Hazard_detection_unit(
-    input ID_EX_MEMread,
-    input [4:0] ID_EX_rt,
-    input [4:0] IF_ID_rs,
-    input [4:0] IF_ID_rt,
-    input [31:0] pc ,
-    input [15:0] immediate,
-    input branch_taken,
-    output reg ID_EX_flush,
-    output reg PC_write,
-     output reg IF_ID_write,
-    output reg [31:0]BTA
+    input ID_EX_MEMread,// instruction will read MEM  (Load use)
+    input [4:0] ID_EX_Rd,
+    input [4:0] IF_ID_RsA,
+    input [4:0] IF_ID_RsB,
+    input [31:0] New_PC_add,
+    input branch,
     
-    );
-    
-    wire [31:0] imm32={{16{immediate[15]}},immediate};
-    wire [31:0] offset = imm32<<2;// convert word to byte
-    reg comparator;
+    output reg    PC_stall,          
+    output reg    IF_ID_stall,       
+    output reg    IF_ID_flush,      
+    output reg    ID_EX_flush,       
+    output reg [31:0] PC_add  
+     );
     always @(*) 
-    begin 
-    PC_write=1'b1;
-    IF_ID_write=1'b1;
+   begin 
+    PC_stall=1'b0;
+    IF_ID_stall=1'b0;
+    IF_ID_flush=1'b0;
     ID_EX_flush=1'b0;
-    BTA=32'd0;
-    
+    PC_add =32'd0;
     // load use hazard 
-    if(ID_EX_MEMread && (ID_EX_rt==IF_ID_rs||ID_EX_rt==IF_ID_rt))// ALU instruction after load instruction  
+    if(ID_EX_MEMread && (ID_EX_Rd==IF_ID_RsA||ID_EX_Rd==IF_ID_RsB))// ALU instruction after load instruction  
     begin
     
-    PC_write=1'b0;
-    IF_ID_write=1'b0;
+    PC_stall=1'b1;
+    IF_ID_stall=1'b1;
     ID_EX_flush=1'b1;
+    
     end
-    
-    else if(branch_taken)
-    begin 
-    // branch target address calculation 
-    BTA= pc+32'd4+offset;
-    
-    PC_write=1'b0;
-    IF_ID_write=1'b0;
-    ID_EX_flush=1'b1;
-      end
+    if(branch)begin
+    PC_add=New_PC_add;
+    IF_ID_flush=1'b1;// taking branch decision at the end of ID stage
     end
-  endmodule  
-    
-    
-    
-    
-    
-    
-    
-   
-    
-    
-    
-   
-              
-                 
-     
-
-
- 
-   
+    end
+    endmodule
